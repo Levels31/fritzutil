16864e5e7f7831f9563d4fbaa9e37d6cde158f00
Levels31 <leonhard.zierer@icloud.com>
2018-03-13 17:33:10 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import hashlib
+import json
+
+page = requests.get("http://fritz.box/login_sid.lua")
+soup = BeautifulSoup(page.text, 'html.parser')
+
+challenge_value = soup.find('challenge').text
+password = challenge_value + '-Gerike19'
+
+md5_value = hashlib.md5(password.encode('utf-16le')).hexdigest()
+page = requests.get("http://fritz.box/login_sid.lua?user=&response=" + challenge_value + "-" + md5_value)
+soup = BeautifulSoup(page.text, 'html.parser')
+sidvalue = soup.find('sid').text
+
+wlan_page = requests.get("http://fritz.box/?sid=" + sidvalue + "&lp=netDev")
+wlan_soup = BeautifulSoup(wlan_page.text, 'html.parser')
+print(wlan_soup.prettify())
+
+json_data=open(file_directory).read()
+
+data = json.loads(json_data)
+print(data)
+
+#print(soup)
+#print(soup.prettify())
